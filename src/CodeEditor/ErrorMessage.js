4231b59a3dd63e09dc067b64b708866c7868bcb9
Marty Jones <murtin.jones@gmail.com>
2017-10-06 16:06:58 -0500

    more formatting

--- src/CodeEditor/ErrorMessage.js
+++ src/CodeEditor/ErrorMessage.js
@@ -17,13 +17,13 @@ const styles = {
 function ErrorMessage (props) {
   const { errorMsg } = props;
   return (
-    <Card style={styles.base}>
+    <Card style={ styles.base }>
       <CardHeader
         title="ERROR:"
-        subtitle={errorMsg}
-        actAsExpander={true}
-        titleColor={'white'}
-        subtitleColor={'white'}
+        subtitle={ errorMsg }
+        actAsExpander={ true }
+        titleColor={ 'white' }
+        subtitleColor={ 'white' }
       />
     </Card>
   )
--- src/CodeEditor/Resources.js
+++ src/CodeEditor/Resources.js
@@ -1,9 +1,3 @@
-/*
- *
- * Resources
- *
- */
-
 import React from 'react';
 import { Row, Col } from 'react-grid-system';
 import Dialog from 'material-ui/Dialog';
@@ -30,8 +24,8 @@ function Resources(props) {
   const actions = [
     <RaisedButton
       label="Close"
-      primary={true}
-      onTouchTap={hide}
+      primary={ true }
+      onTouchTap={ hide }
     />,
   ];
   const len = resources.length;
@@ -43,59 +37,53 @@ function Resources(props) {
     if(resource.code){
       return resource.code
     } else if(resource.url) {
-      return (
-        <a href={resource.url} target="_blank" style={styles.aTag}>{resource.title}</a>
-      )
+      return <a href={ resource.url } target="_blank" style={ styles.aTag }>{ resource.title }</a>
     }
   }
 
   return (
     <Dialog
       title="Python Resources!"
-      actions={actions}
-      modal={false}
-      open={show}
-      onRequestClose={hide}
-      contentStyle={styles.base}
-      autoScrollBodyContent={true}
+      actions={ actions }
+      modal={ false }
+      open={ show }
+      onRequestClose={ hide }
+      contentStyle={ styles.base }
+      autoScrollBodyContent={ true }
     >
       <Row>
-        <Col md={6}>
-          {
-            leftCol.map((resource, index) => {
-              return (
-                <Card key={resource.title} style={styles.cards}>
-                  <CardHeader
-                    title={resource.title}
-                    subtitle={resource.description}
-                  />
-                  <CardText>
-                    <pre>
-                      {resource.code}
-                    </pre>
-                  </CardText>
-                </Card>
-              )
-            })
+        <Col md={ 6 }>
+          { leftCol.map((resource, index) => {
+            return (
+              <Card key={ resource.title } style={ styles.cards }>
+                <CardHeader
+                  title={ resource.title }
+                  subtitle={ resource.description }
+                />
+                <CardText>
+                  <pre>
+                    { resource.code }
+                  </pre>
+                </CardText>
+              </Card>
+            )} )
           }
         </Col>
-        <Col md={6}>
-          {
-            rightCol.map((resource, index) => {
-              return (
-                <Card key={resource.title} style={styles.cards}>
-                  <CardHeader
-                    title={resource.title}
-                    subtitle={resource.description}
-                  />
-                  <CardText>
-                    <pre>
-                      {getResourceCodeOrURL(resource)}
-                    </pre>
-                  </CardText>
-                </Card>
-              )
-            })
+        <Col md={ 6 }>
+          { rightCol.map((resource, index) => {
+            return (
+              <Card key={resource.title} style={styles.cards}>
+                <CardHeader
+                  title={resource.title}
+                  subtitle={resource.description}
+                />
+                <CardText>
+                  <pre>
+                    {getResourceCodeOrURL(resource)}
+                  </pre>
+                </CardText>
+              </Card>
+            )} )
           }
         </Col>
       </Row>
