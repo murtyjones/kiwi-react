5af8b3535a00bb60ad2072ac51f12c4966f74c0c
Marty Jones <murtin.jones@gmail.com>
2017-10-17 15:38:27 -0500

    rm localstorage on widget

--- src/Dashboard/UserProjectWidget.js
+++ src/Dashboard/UserProjectWidget.js
@@ -3,14 +3,6 @@ import { Link } from 'react-router-dom'
 
 let UserProjectWidget = props => {
   const { project: { title, _id } } = props
-  //not ideal, but for the moment I need a quick and dirty way to store the title
-  //alternatives are redux and maybe an option to push it through link, but I a cursory glance doesnt find it.
-  //or possibly we dont use link...hmmm...
-  //this is an annoying architecture question
-  //--peter
-
-  localStorage.setItem("projectTitle", title)
-  localStorage.setItem("projectId", _id)
 
   return (
     <div>
