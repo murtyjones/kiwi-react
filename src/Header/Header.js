34fd107143b89440dc43978c380e07eefa04f74d
Latif Masud <latif.masud@hotmail.com>
2017-10-21 12:40:26 -0500

    Initial scaffold of header.

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,6 +4,12 @@ import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signout } from '../actions'
 
+//Material ui stuf
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
+import AppBar from 'material-ui/AppBar';
+
 class Header extends Component {
   constructor(props) {
     super(props)
@@ -20,10 +26,27 @@ class Header extends Component {
     })
   }
 
+  navigateTo = (loc) => {
+    console.log(loc)
+    this.props.history.push(loc)
+
+  }
+
   render() {
+    const styles = {
+      backgroundColor: "#ffffff"
+    };
+
     const { isLoggedIn } = this.props
-    const renderLogout = isLoggedIn ? (<div onClick={ this.logout }>Log out</div>) : null
-    return renderLogout
+    const renderLogout = isLoggedIn ? ({"title": "Logout", "path": "/logout"}) : ({"title": "Login", "path": "/login"})
+
+    return (
+      <AppBar
+        styles={styles}
+        title="Kiwi Compute"
+        iconElementRight={<FlatButton label={renderLogout.title} onClick={() => this.navigateTo(renderLogout.path)}/>}
+      />
+    )
   }
 }
 
