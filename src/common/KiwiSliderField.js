413b224acc3ce93ce9f3a44a5bbfaac3ddf7f73c
Marty Jones <murtin.jones@gmail.com>
2017-10-26 09:58:15 -0500

    load in slider value if available

--- src/common/KiwiSliderField.js
+++ src/common/KiwiSliderField.js
@@ -10,6 +10,12 @@ class KiwiSelectField extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(this.props.input.value !== nextProps.input.value) {
+      this.setState({ value: nextProps.input.value })
+    }
+  }
+
   render() {
     const { min, max, defaultValue, step, onDragStop, input, label, type, meta: { touched, error } } = this.props
     const { value } = this.state
@@ -22,6 +28,7 @@ class KiwiSelectField extends Component {
         <div>
           <Slider
             defaultValue={ defaultValue }
+            value={ value }
             min={ min }
             max={ max }
             step={ step }
