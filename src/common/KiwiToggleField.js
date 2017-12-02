90d2e66426170c297b1519782faaaeace55d808a
Marty Jones <murtin.jones@gmail.com>
2017-12-01 18:09:05 -0600

    try new toggle component

--- src/common/KiwiToggleField.js
+++ src/common/KiwiToggleField.js
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Toggle } from 'material-ui'
 
-const KiwiToggleField = ({ children, input, label, type, meta, ...custom }) => {
+const _KiwiToggleField = ({ children, input, label, type, meta, ...custom }) => {
   return [
     <label key={ `label-${1}` }>{ label }</label>
     ,
     <Toggle
       key={ `label-${2}` }
       toggled={ !!input.value }
-      onBlur={ (e) => { input.onChange(e.target.checked) } }
-      // onToggle={ (e, v) => {
-      //   console.log(v)
-      //   input.onChange(v)
-      // } }
+      // onClick={ (e) => { input.onChange(e.target.checked) } }
+      onToggle={ (e, v) => {
+        console.log(v)
+        input.onChange(v)
+      } }
       { ... custom }
     />
   ]
 
+}
+
+
+class KiwiToggleField extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { children, input, label, type, meta, custom } = this.props
+    console.log(input)
+    console.log(custom)
+    return [
+      <label key={ `label-${1}` }>{ label }</label>
+      ,
+      <Toggle
+        key={ `label-${2}` }
+        toggled={ !!input.value }
+        onBlur={ (e) => { input.onChange(e.target.checked) } }
+        // onToggle={ (e, v) => {
+        //   console.log('hiya')
+        //   input.onChange(v)
+        //   this.handleToggle(v)
+        // } }
+        // { ... custom }
+      />
+    ]
+  }
 }
 
 export default KiwiToggleField
