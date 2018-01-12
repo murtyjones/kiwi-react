4bfb1fb671ab735000bcc5332c3047058f7bcb1a
Marty Jones <murtin.jones@gmail.com>
2018-01-11 23:17:16 -0600

    more tests

--- test/__tests__/integration/Lessons/Lessons.test.js
+++ test/__tests__/integration/Lessons/Lessons.test.js
@@ -35,7 +35,7 @@ describe('Lessons', () => {
   let chesterAdminIdToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlJqSTNNRVV4UlRoR1JVSXlNRVEzUkVGQk1rSXlSa1JGTUVZek16RkVNamRGT1RaR1FUVTRSUSJ9.eyJodHRwczovL2ludGVncmF0aW9uLXRlc3Qua2l3aWNvbXB1dGUuY29tL2FwcF9tZXRhZGF0YSI6eyJ1c2VySWQiOiI1YTI2MmYzY2Q3OTk3NDdiMjU3YWNlNDEiLCJyb2xlcyI6W3siaXNBZG1pbiI6dHJ1ZX1dfSwibmlja25hbWUiOiJjaGVzdGVydGhldGVzdGVyIiwibmFtZSI6ImNoZXN0ZXJ0aGV0ZXN0ZXJAa2l3aWNvbXB1dGUuY29tIiwicGljdHVyZSI6Imh0dHBzOi8vcy5ncmF2YXRhci5jb20vYXZhdGFyLzkwYTk5N2ZlMWNlNWM4ZTAyNWQ4NjY3YzI0ZWY1Nzc5P3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGY2gucG5nIiwidXBkYXRlZF9hdCI6IjIwMTctMTItMTNUMDA6MzU6NTcuNTkzWiIsImVtYWlsIjoiY2hlc3RlcnRoZXRlc3RlckBraXdpY29tcHV0ZS5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6Ly9raXdpLWludGVncmF0aW9uLXRlc3QuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVhMjYyZjNjNDUxNTc3MTFiZTgyYmFjMCIsImF1ZCI6Ik5nakxRdFJiUDdXZHNfX1J1MTBnN3FQbkpRVDdMbjZaIiwiaWF0IjoxNTEzMTI1MzU3LCJleHAiOjE1MTMxNjEzNTd9.PXmHGo_Q1mtvw_feGF1NY3uxjsBPEKTPYikKHoiLoVGjw1jmiwztZ7okKddfk8s8oqDeH7Htrfl7vqbDSA8EedZX9aExNAwu3MgIW3p2LKp0STsp9Zuz1NlrAIX__nsnB22Bhn8tvbYyRFqyhtZ1__lmg-w2ouQ3lZuf00Xd-V5QEnzuBbykSi3IcVbx1oUdqqvi7bz-U6RRP_cBMhbgsbD1pEDcX5vnLQMqWeZHweTq43tpwQcKciNz21LMB4fPs5LbIPU3mglkkfObJl4x4k0BdEYjZ0Qn42VMRI7VSpRibP0DfRvWhx3edp0mX0zDGm08R8n7LUuXYxfZvGfocw'
     , chesterAdminUserId = '5a262f3cd799747b257ace41'
     , lesson1, lesson2, lesson3, lesson4
-    , userLesson1, userLesson2
+    , userLesson1, userLesson2, userLesson3
     , lessons
     , userLessons
     , lessonOrder
@@ -91,10 +91,15 @@ describe('Lessons', () => {
       , hasBeenCompleted: true
     }
     userLesson2 = {
-      _id: 'userLesson2',
-      lessonId: lesson2._id
+      _id: 'userLesson2'
+      , lessonId: lesson2._id
+      , hasBeenCompleted: true
     }
-    userLessons = [ userLesson1, userLesson2 ]
+    userLesson3 = {
+      _id: 'userLesson3',
+      lessonId: lesson3._id
+    }
+    userLessons = [ userLesson1, userLesson2, userLesson3 ]
     lessonOrder = { order: [ lesson1._id, lesson2._id, lesson3._id, lesson4._id ] }
     setupStore = () => {
       ({store, dispatchSpy} = setupIntegrationTest(notCombined, router))
@@ -190,7 +195,10 @@ describe('Lessons', () => {
             ...lesson2
             , userLesson: userLesson2
           },
-          lesson3,
+          {
+            ...lesson3
+            , userLesson: userLesson3
+          },
           lesson4
         ]
         expect(component.find('LessonMap').props().mapLessons).toEqual(expectedMapLessons)
@@ -209,10 +217,10 @@ describe('Lessons', () => {
         expect(component.find(`Circle`).length).toBe(lessons.length * 2)
       })
 
-      it('should render one Text element with the order as content for each lesson that has a userLesson', async () => {
+      it('should render one Text element with the order as content for each lesson that is available', async () => {
         expect(component.find(`Text[text=${1}]`).length).toBe(1)
         expect(component.find(`Text[text=${2}]`).length).toBe(1)
-        expect(component.find(`Text[text=${3}]`).length).toBe(0)
+        expect(component.find(`Text[text=${3}]`).length).toBe(1)
         expect(component.find(`Text[text=${4}]`).length).toBe(0)
       })
 
@@ -220,10 +228,13 @@ describe('Lessons', () => {
         expect(component.find(`Arc`).length).toBe(lessons.length * 2)
       })
 
-      it('should render one Line for the one completed lesson', async () => {
-        expect(component.find(`Line`).length).toBe(1)
+      it('should render one Line for the two completed lessons', async () => {
+        expect(component.find(`Line`).length).toBe(2)
       })
 
+      it('should render one Path (lock icon) for the one locked lesson', async () => {
+        expect(component.find(`Path`).length).toBe(1)
+      })
 
     })
 
