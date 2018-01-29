a7e9ee2234f0c3f6b44db09a06b0d9e34f8d0180
Marty Jones <murtin.jones@gmail.com>
2018-01-28 22:25:55 -0600

    whoops need actual points

--- src/constants/LESSON_MAP_POINTS.js
+++ src/constants/LESSON_MAP_POINTS.js
@@ -12,31 +12,31 @@ const _calculateGoToPoint = (y_point) => {
 const LESSON_MAP_POINTS = {
   CIRCLE_1_Y: 16.5
   , CIRCLE_1_X: 13
-  , CIRCLE_1_GOTO: _calculateGoToPoint(67)
+  , CIRCLE_1_GOTO: _calculateGoToPoint(15) // intentional mismatch
 
   , CIRCLE_2_Y: 32.5
   , CIRCLE_2_X: 63
-  , CIRCLE_2_GOTO: _calculateGoToPoint(67)
+  , CIRCLE_2_GOTO: _calculateGoToPoint(32.5)
 
   , CIRCLE_3_Y: 67
   , CIRCLE_3_X: 80
-  , CIRCLE_3_GOTO: _calculateGoToPoint(32.5)
+  , CIRCLE_3_GOTO: _calculateGoToPoint(67)
 
   , CIRCLE_4_Y: 106.5
   , CIRCLE_4_X: 26
-  , CIRCLE_4_GOTO: _calculateGoToPoint(67)
+  , CIRCLE_4_GOTO: _calculateGoToPoint(106.5)
 
   , CIRCLE_5_Y: 125
   , CIRCLE_5_X: 33
-  , CIRCLE_5_GOTO: _calculateGoToPoint(67)
+  , CIRCLE_5_GOTO: _calculateGoToPoint(125)
 
   , CIRCLE_6_Y: 155
   , CIRCLE_6_X: 66
-  , CIRCLE_6_GOTO: _calculateGoToPoint(67)
+  , CIRCLE_6_GOTO: _calculateGoToPoint(155)
 
   , CIRCLE_7_Y: 175
   , CIRCLE_7_X: 18
-  , CIRCLE_7_GOTO: _calculateGoToPoint(67)
+  , CIRCLE_7_GOTO: _calculateGoToPoint(175)
 }
 
 export default LESSON_MAP_POINTS
