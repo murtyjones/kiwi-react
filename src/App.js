9e11c26e5b2921cce12484816c58ce6dd54d114c
Marty Jones <murtin.jones@gmail.com>
2018-01-29 18:38:52 -0600

    use webfontloader

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import * as T from 'prop-types'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import "babel-polyfill"
+import 'babel-polyfill'
 import 'raf/polyfill' // polyfill
+import WebFont from 'webfontloader'
 
 import Routes from './Routes'
+
+WebFont.load({
+  google: {
+    families: ['Arvo', 'Roboto']
+  }
+})
 
 class App extends Component {
 
