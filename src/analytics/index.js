2c4078014b57fb651b2352097f42cfe2e4c89efa
Marty Jones <murtin.jones@gmail.com>
2018-06-14 23:34:56 -0500

    hm

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -4,10 +4,10 @@ const options = {
   gaOptions: {}
 }
 
-if (process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
   options.debug = true
   options.gaOptions.cookieDomain = 'none'
-}
+// }
 
 ReactGA.initialize('UA-120446838-1', options)
 
