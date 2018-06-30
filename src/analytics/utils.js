d804a02c49ed4dfb5a3ce118521d036da40d077d
marty jones <murtin.jones@gmail.com>
2018-06-30 17:13:18 -0500

    functionize

--- src/analytics/utils.js
+++ src/analytics/utils.js
@@ -1,6 +1,8 @@
 import ReactGA from './index'
 
-const performIfProd = action => process.env.NODE_ENV === 'production' ? action() : () => null
+const performIfProd = action => process.env.NODE_ENV === 'production'
+    ? () => action()
+    : () => null
 
 export const trackPage = performIfProd((page, options) => {
     ReactGA.set({
