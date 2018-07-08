8e1ffda9b4bfbb4e10f702be16ae06e696b963de
marty jones <murtin.jones@gmail.com>
2018-07-08 11:54:57 -0500

    Revert "tag manger beginnings"
    This reverts commit 182e1b80fcedf23e9986f73a8647dd79ecbe4aac.

--- src/Routes/PlainRoute.js
+++ src/Routes/PlainRoute.js
@@ -1,15 +1,10 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Route from 'react-router-dom/Route'
 import withTracker from '../hocs/withTracker'
-import { GoogleTagManager } from '../hocs/googleTagManager'
 
 const PlainRoute = props => {
-
   return (
-    <Fragment>
-      <GoogleTagManager dataLayer={ props.path } />
-      <Route { ...props } />
-    </Fragment>
+    <Route { ...props } />
   )
 }
 
