f00237157512bcda34d306de8fd2c84a5fab8ccf
marty jones <murtin.jones@gmail.com>
2018-07-10 17:32:03 -0500

    map variable options correctly

--- src/admin/AddOrEditLesson/SlideConfigs/InputSuccessCriteria.js
+++ src/admin/AddOrEditLesson/SlideConfigs/InputSuccessCriteria.js
@@ -176,7 +176,7 @@ const UserDefinedVariable = ({ eachSlideRef, variableOptions, onDeleteCrition })
         component={ KiwiSelectField }
         style={ { width: '100%' } }
         containerStyle={ styles.variable }
-        options={ variableOptions }
+        options={ variableOptions.map(e => ({ value: e._id, label: e.name })) }
       />
       <DeleteButton onClick={ onDeleteCrition } />
     </div>
