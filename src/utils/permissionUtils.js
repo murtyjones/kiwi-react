9b9d46f8b3179717b4230dda5c505807be98bf6a
Marty Jones <murtin.jones@gmail.com>
2018-08-03 09:23:11 -0500

    KIWA-308

--- src/utils/permissionUtils.js
+++ src/utils/permissionUtils.js
@@ -8,7 +8,7 @@ export const isSubscriptionValid = subscription => {
   if (!subscription)
     return false
 
-  if (!subscription.status !== SUBSCRIPTION_STATUSES.ACTIVE) {
+  if (subscription.status !== SUBSCRIPTION_STATUSES.ACTIVE) {
     if (
       !subscription.cancelAtPeriodEnd ||
       isPastPastPeriodEnd(subscription)
