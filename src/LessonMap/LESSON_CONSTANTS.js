02e61859fd6c3f960ad0f15d71af3f7ec6ef53ad
Marty Jones <murtin.jones@gmail.com>
2018-08-04 12:31:03 -0500

    uncover

--- src/LessonMap/LESSON_CONSTANTS.js
+++ src/LessonMap/LESSON_CONSTANTS.js
@@ -2,30 +2,30 @@ export const lessonBubbleDisplayDataBySection = [
   [ // sectionIndex 0
     { x: 19, y: 36, left: undefined },  // lessonIndex 0
     { x: 19, y: 48, left: undefined },
-    // { x: 18, y: 63, left: undefined },
-    // { x: 21, y: 74, left: true      },
-    // { x: 32, y: 75, left: true      },
-    // { x: 43, y: 74, left: undefined },
-    // { x: 43, y: 59, left: undefined },
-    // { x: 38, y: 45, left: undefined },
-    // { x: 41, y: 25, left: true      },
-    // { x: 47, y: 19, left: true      },
-    // { x: 54, y: 14, left: undefined },
-    // { x: 61, y: 13, left: undefined },
-    // { x: 68, y: 14, left: undefined },
-    // { x: 74, y: 19, left: undefined },
-    // { x: 79, y: 27, left: undefined },
-    // { x: 82, y: 37, left: undefined },
-    // { x: 80, y: 49, left: undefined },
-    // { x: 80, y: 62, left: undefined },
-    // { x: 83, y: 75, left: undefined },  // lessonIndex 18
+    { x: 18, y: 63, left: undefined },
+    { x: 21, y: 74, left: true      },
+    { x: 32, y: 75, left: true      },
+    { x: 43, y: 74, left: undefined },
+    { x: 43, y: 59, left: undefined },
+    { x: 38, y: 45, left: undefined },
+    { x: 41, y: 25, left: true      },
+    { x: 47, y: 19, left: true      },
+    { x: 54, y: 14, left: undefined },
+    { x: 61, y: 13, left: undefined },
+    { x: 68, y: 14, left: undefined },
+    { x: 74, y: 19, left: undefined },
+    { x: 79, y: 27, left: undefined },
+    { x: 82, y: 37, left: undefined },
+    { x: 80, y: 49, left: undefined },
+    { x: 80, y: 62, left: undefined },
+    { x: 83, y: 75, left: undefined },  // lessonIndex 18
   ],
   [ // sectionIndex 1
     { x: 22, y: 52, left: undefined },  // lessonIndex 19
     { x: 80, y: 77, left: undefined },
-    // { x: 88, y: 72, left: undefined },
-    // { x: 86, y: 42, left: undefined },
-    // { x: 92, y: 47, left: undefined },  // lessonIndex 23
+    { x: 88, y: 72, left: undefined },
+    { x: 86, y: 42, left: undefined },
+    { x: 92, y: 47, left: undefined },  // lessonIndex 23
   ]
   // don't add section here till truly ready
 ]
