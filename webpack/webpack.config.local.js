fd21d408c8e7f39546a8c8c32fef4deb17fb68df
Marty Jones <murtin.jones@gmail.com>
2018-08-25 09:41:40 -0500

    random-words

--- webpack/webpack.config.local.js
+++ webpack/webpack.config.local.js
@@ -28,7 +28,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules\/(?!(random-words)\/).*/,
         loader: 'babel-loader'
       }, {
         test: /\.json$/,
