a2f7994d2f184393dab0d4d886add6d0ca802dfe
Marty Jones <murtin.jones@gmail.com>
2018-08-25 07:51:35 -0500

    env

--- src/Main.js
+++ src/Main.js
@@ -35,29 +35,3 @@ if (module.hot) {
     )
   })
 }
-
-window.addEventListener('error', function(e) {
-  var errorText = [
-    e.message,
-    'URL: ' + e.filename,
-    'Line: ' + e.lineno + ', Column: ' + e.colno,
-    'Stack: ' + (e.error && e.error.stack || '(no stack trace)')
-  ].join('\n');
-
-  // Example: log errors as visual output into the host page.
-  // Note: you probably don’t want to show such errors to users, or
-  //       have the errors get indexed by Googlebot; however, it may
-  //       be a useful feature while actively debugging the page.
-  var DOM_ID = 'rendering-debug-pre';
-  if (!document.getElementById(DOM_ID)) {
-    var log = document.createElement('pre');
-    log.id = DOM_ID;
-    log.style.whiteSpace = 'pre-wrap';
-    log.textContent = errorText;
-    if (!document.body) document.body = document.createElement('body');
-    document.body.insertBefore(log, document.body.firstChild);
-  } else {
-    document.getElementById(DOM_ID).textContent += '\n\n' + errorText;
-  }
-
-});
