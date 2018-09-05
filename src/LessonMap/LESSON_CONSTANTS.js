1479adfbb1a0ef0f76fb11b18d61ba3d8fa7ad35
Marty Jones <murtin.jones@gmail.com>
2018-09-04 21:19:50 -0500

    comment

--- src/LessonMap/LESSON_CONSTANTS.js
+++ src/LessonMap/LESSON_CONSTANTS.js
@@ -1,24 +1,24 @@
 export const lessonBubbleDisplayDataBySection = [
   [ // sectionIndex 0
     { x: 19, y: 36, left: true      },  // lessonIndex 0
-    // { x: 19, y: 48, left: true      },
-    // { x: 18, y: 63, left: true      },
-    // { x: 21, y: 74, left: true      },
-    // { x: 32, y: 75, left: true      },
-    // { x: 43, y: 74, left: true      },
-    // { x: 43, y: 59, left: true      },
-    // { x: 38, y: 45, left: true      },
-    // { x: 41, y: 25, left: true      },
-    // { x: 47, y: 19, left: true      },
-    // { x: 54, y: 14, left: undefined },
-    // { x: 61, y: 13, left: undefined },
-    // { x: 68, y: 14, left: undefined },
-    // { x: 74, y: 19, left: undefined },
-    // { x: 79, y: 27, left: undefined },
-    // { x: 82, y: 37, left: undefined },
-    // { x: 80, y: 49, left: undefined },
-    // { x: 80, y: 62, left: undefined },
-    // { x: 83, y: 75, left: undefined },  // lessonIndex 18
+    { x: 19, y: 48, left: true      },
+    { x: 18, y: 63, left: true      },
+    { x: 21, y: 74, left: true      },
+    { x: 32, y: 75, left: true      },
+    { x: 43, y: 74, left: true      },
+    { x: 43, y: 59, left: true      },
+    { x: 38, y: 45, left: true      },
+    { x: 41, y: 25, left: true      },
+    { x: 47, y: 19, left: true      },
+    { x: 54, y: 14, left: undefined },
+    { x: 61, y: 13, left: undefined },
+    { x: 68, y: 14, left: undefined },
+    { x: 74, y: 19, left: undefined },
+    { x: 79, y: 27, left: undefined },
+    { x: 82, y: 37, left: undefined },
+    { x: 80, y: 49, left: undefined },
+    { x: 80, y: 62, left: undefined },
+    { x: 83, y: 75, left: undefined },  // lessonIndex 18
   ],
   [ // sectionIndex 1
     { x: 22, y: 52, left: undefined },  // lessonIndex 19
